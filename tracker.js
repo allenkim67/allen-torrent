545c831fd509d7a5fe87dd08b8ff4d2ecbfefbf7
Allen Kim <allenkim67@gmail.com>
2016-05-06 11:46:41 +0700

    add connection request and response functions

--- tracker.js
+++ tracker.js
@@ -3,6 +3,7 @@
 const dgram = require('dgram');
 const Buffer = require('buffer').Buffer;
 const urlParse = require('url').parse;
+const crypto = require('crypto');
 
 module.exports.getPeers = (torrent, callback) => {
   const socket = dgram.createSocket('udp4');
@@ -37,11 +38,25 @@ function respType(resp) {
 }
 
 function buildConnReq() {
-  // ...
+  const buf = Buffer.allocUnsafe(16);
+
+  // connection id
+  buf.writeUInt32BE(0x417, 0);
+  buf.writeUInt32BE(0x27101980, 4);
+  // action
+  buf.writeUInt32BE(0, 8);
+  // transaction id
+  crypto.randomBytes(4).copy(buf, 12);
+
+  return buf;
 }
 
 function parseConnResp(resp) {
-  // ...
+  return {
+    action: resp.readUInt32BE(0),
+    transactionId: resp.readUInt32BE(4),
+    connectionId: resp.slice(8)
+  }
 }
 
 function buildAnnounceReq(connId) {
