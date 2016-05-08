d4065b4875b64644841b37cf004decdf0a648faa
Allen Kim <allenkim67@gmail.com>
2016-05-08 04:05:08 -0700

    add respType function to tracker.js

--- tracker.js
+++ tracker.js
@@ -36,7 +36,9 @@ function udpSend(socket, message, rawUrl, callback=()=>{}) {
 }
 
 function respType(resp) {
-  // ...
+  const action = resp.readUInt32BE(0);
+  if (action === 0) return 'connect';
+  if (action === 1) return 'announce';
 }
 
 function buildConnReq() {
