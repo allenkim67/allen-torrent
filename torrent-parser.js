460baa163e6445ad51a1eab1da43237289310462
Allen Kim <allenkim67@gmail.com>
2016-05-08 04:03:48 -0700

    add infoHash and size functions to torrent-parser

--- torrent-parser.js
+++ torrent-parser.js
@@ -2,15 +2,22 @@
 
 const fs = require('fs');
 const bencode = require('bencode');
+const crypto = require('crypto');
+const bignum = require('bignum');
 
 module.exports.open = (filepath) => {
   return bencode.decode(fs.readFileSync(filepath));
 };
 
-module.exports.size = torrent => {
-  // ...
+module.exports.infoHash = torrent => {
+  const info = bencode.encode(torrent.info);
+  return crypto.createHash('sha1').update(info).digest();
 };
 
-module.exports.infoHash = torrent => {
-  // ...
+module.exports.size = torrent => {
+  const size = torrent.info.files ?
+    torrent.info.files.map(file => file.length).reduce((a, b) => a + b) :
+    torrent.info.length;
+
+  return bignum.toBuffer(size, {size: 8});
 };
