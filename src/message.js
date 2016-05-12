c7c204dacf2d8e0f6312bd725956d7e22e8e0c42
Allen Kim <allenkim67@gmail.com>
2016-05-11 23:43:49 -0700

    add message.js

---
+++ src/message.js
@@ -0,0 +1,137 @@
+'use strict';
+
+const Buffer = require('buffer').Buffer;
+const torrentParser = require('./torrent-parser');
+const util = require('./util');
+
+module.exports.buildHandshake = torrent => {
+  const buf = Buffer.alloc(68);
+  // pstrlen
+  buf.writeUInt8(19, 0);
+  // pstr
+  buf.write('BitTorrent protocol', 1);
+  // reserved
+  buf.writeUInt32BE(0, 20);
+  buf.writeUInt32BE(0, 24);
+  // info hash
+  torrentParser.infoHash(torrent).copy(buf, 28);
+  // peer id
+  util.genId().copy(buf, 48);
+  return buf;
+};
+
+module.exports.buildKeepAlive = () => Buffer.alloc(4);
+
+module.exports.buildChoke = () => {
+  const buf = Buffer.alloc(5);
+  // length
+  buf.writeUInt32BE(1, 0);
+  // id
+  buf.writeUInt8(0, 4);
+  return buf;
+};
+
+module.exports.buildUnchoke = () => {
+  const buf = Buffer.alloc(5);
+  // length
+  buf.writeUInt32BE(1, 0);
+  // id
+  buf.writeUInt8(1, 4);
+  return buf;
+};
+
+module.exports.buildInterested = () => {
+  const buf = Buffer.alloc(5);
+  // length
+  buf.writeUInt32BE(1, 0);
+  // id
+  buf.writeUInt8(2, 4);
+  return buf;
+};
+
+module.exports.buildUninterested = () => {
+  const buf = Buffer.alloc(5);
+  // length
+  buf.writeUInt32BE(1, 0);
+  // id
+  buf.writeUInt8(3, 4);
+  return buf;
+};
+
+module.exports.buildHave = payload => {
+  const buf = Buffer.alloc(9);
+  // length
+  buf.writeUInt32BE(5, 0);
+  // id
+  buf.writeUInt8(4, 4);
+  // piece index
+  buf.writeUInt32BE(payload, 5);
+  return buf;
+};
+
+module.exports.buildBitfield = bitfield => {
+  const buf = Buffer.alloc(14);
+  // length
+  buf.writeUInt32BE(payload.length + 1, 0);
+  // id
+  buf.writeUInt8(5, 4);
+  // bitfield
+  bitfield.copy(buf, 5);
+  return buf;
+};
+
+module.exports.buildRequest = payload => {
+  const buf = Buffer.alloc(17);
+  // length
+  buf.writeUInt32BE(13, 0);
+  // id
+  buf.writeUInt8(6, 4);
+  // piece index
+  buf.writeUInt32BE(payload.index, 5);
+  // begin
+  buf.writeUInt32BE(payload.begin, 9);
+  // length
+  buf.writeUInt32BE(payload.length, 13);
+  return buf;
+};
+
+module.exports.buildPiece = payload => {
+  const buf = Buffer.alloc(payload.block.length + 13);
+  // length
+  buf.writeUInt32BE(payload.block.length + 9, 0);
+  // id
+  buf.writeUInt8(7, 4);
+  // piece index
+  buf.writeUInt32BE(payload.index, 5);
+  // begin
+  buf.writeUInt32BE(payload.begin, 9);
+  // block
+  payload.block.copy(buf, 13);
+  return buf;
+};
+
+module.exports.buildCancel = payload => {
+  const buf = Buffer.alloc(17);
+  // length
+  buf.writeUInt32BE(13, 0);
+  // id
+  buf.writeUInt8(8, 4);
+  // piece index
+  buf.writeUInt32BE(payload.index, 5);
+  // begin
+  buf.writeUInt32BE(payload.begin, 9);
+  // length
+  buf.writeUInt32BE(payload.length, 13);
+  return buf;
+};
+
+module.exports.buildPort = payload => {
+  const buf = Buffer.alloc(7);
+  // length
+  buf.writeUInt32BE(3, 0);
+  // id
+  buf.writeUInt8(9, 4);
+  // listen-port
+  buf.writeUInt16BE(payload, 5);
+  return buf;
+};
