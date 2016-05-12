7f90bbb927444b41cd26900f4fed4f23f74686d8
Allen Kim <allenkim67@gmail.com>
2016-05-11 21:43:55 -0700

    add onWholeMsg function

--- src/download.js
+++ src/download.js
@@ -15,7 +15,24 @@ function download(peer) {
   socket.connect(peer.port, peer.ip, () => {
     // socket.write(...) write a message here
   });
-  socket.on('data', data => {
+  onWholeMsg(socket, data => {
     // handle response here
+  });
+}
+
+function onWholeMsg(socket, callback) {
+  let savedBuf = Buffer.alloc(0);
+  let handshake = true;
+
+  socket.on('data', recvBuf => {
+    // msgLen calculates the length of a whole message
+    const msgLen = () => handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4;
+    savedBuf = Buffer.concat([savedBuf, recvBuf]);
+
+    while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+      callback(savedBuf.slice(0, msgLen()));
+      savedBuf = savedBuf.slice(msgLen());
+      handshake = false;
+    }
   });
 }
