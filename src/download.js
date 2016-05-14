5da16dfd3e5c567d83ff20b6a361ea921100c0f7
Allen Kim <allenkim67@gmail.com>
2016-05-14 12:02:53 -0700

    add non-handshake message handlers

--- src/download.js
+++ src/download.js
@@ -37,10 +37,40 @@ function onWholeMsg(socket, callback) {
 }
 
 function msgHandler(msg, socket) {
-  if (isHandshake(msg)) socket.write(message.buildInterested());
+  if (isHandshake(msg)) {
+    socket.write(message.buildInterested());
+  } else {
+    const m = message.parse(msg);
+
+    if (m.id === 0) chokeHandler();
+    if (m.id === 1) unchokeHandler();
+    if (m.id === 4) haveHandler(m.payload);
+    if (m.id === 5) bitfieldHandler(m.payload);
+    if (m.id === 7) pieceHandler(m.payload);
+  }
 }
 
 function isHandshake(msg) {
   return msg.length === msg.readUInt8(0) + 49 &&
          msg.toString('utf8', 1, 20) === 'BitTorrent protocol';
+}
+
+function chokeHandler() {
+  // ...
+}
+
+function unchokeHandler() {
+  // ...
+}
+
+function haveHandler() {
+  // ...
+}
+
+function bitfieldHandler() {
+  // ...
+}
+
+function pieceHandler() {
+  // ...
 }
--- src/message.js
+++ src/message.js
@@ -135,3 +135,22 @@ module.exports.buildPort = payload => {
   buf.writeUInt16BE(payload, 5);
   return buf;
 };
+
+module.exports.parse = msg => {
+  const id = msg.length > 4 ? msg.readInt8(4) : null;
+  let payload = msg.length > 5 ? msg.slice(5) : null;
+  if (id === 6 || id === 7 || id === 8) {
+    const rest = payload.slice(8);
+    payload = {
+      index: payload.readInt32BE(0),
+      begin: payload.readInt32BE(4)
+    };
+    payload[id === 7 ? 'block' : 'length'] = rest;
+  }
+
+  return {
+    size : msg.readInt32BE(0),
+    id : id,
+    payload : payload
+  }
+};
