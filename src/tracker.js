8fc26e9f98b7bb723bce59099e30006f882fb670
Tushar Bajaj <tbajaj@iitg.ac.in>
2020-12-17 03:08:02 +0530

    changed 80 to 84

--- src/tracker.js
+++ src/tracker.js
@@ -85,7 +85,7 @@ function buildAnnounceReq(connId, torrent, port=6881) {
   // event
   buf.writeUInt32BE(0, 80);
   // ip address
-  buf.writeUInt32BE(0, 80);
+  buf.writeUInt32BE(0, 84);
   // key
   crypto.randomBytes(4).copy(buf, 88);
   // num want
